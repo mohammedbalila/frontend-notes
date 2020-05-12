b46483a28a294a78c53b683087a8326d4b4b2682
mustafabalila <hitsugayatoshiro899@gmail.com>
2020-05-13 01:50:03 +0200

    meta

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,21 @@
 module.exports = {
-  transpileDependencies: [
-    'vuetify',
-  ],
+  transpileDependencies: ['vuetify'],
+  pwa: {
+    name: 'Diarme',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'indigo',
+
+    favicon32: 'img/icons/favicon-32x32.png',
+    favicon16: 'img/icons/favicon-16x16.png',
+    appleTouchIcon: 'img/icons/apple-touch-icon.png',
+    maskIcon: 'img/icons/safari-pinned-tab.svg',
+    msTileImage: 'img/icons/msapplication-icon-144x144.png',
+
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: 'dev/sw.js',
+      // ...other Workbox options...
+    },
+  },
 };
