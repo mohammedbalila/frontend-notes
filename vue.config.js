ff98daad87764eb6986b090329c71920c81f8331
mustafabalila <hitsugayatoshiro899@gmail.com>
2020-05-13 02:04:41 +0200

    meta

--- vue.config.js
+++ vue.config.js
@@ -10,12 +10,5 @@ module.exports = {
     appleTouchIcon: 'img/icons/apple-touch-icon.png',
     maskIcon: 'img/icons/safari-pinned-tab.svg',
     msTileImage: 'img/icons/msapplication-icon-144x144.png',
-
-    workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-      // swSrc is required in InjectManifest mode.
-      swSrc: 'dev/sw.js',
-      // ...other Workbox options...
-    },
   },
 };
